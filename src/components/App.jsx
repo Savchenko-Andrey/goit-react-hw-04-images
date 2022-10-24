92aa0451a5f1266bfcc85b35a4c095782fb6d803
Savchenko-Andrey <dracxm95@gmail.com>
2022-10-24 13:08:31 +0300

    change 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,108 +1,81 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ClipLoader from 'react-spinners/ClipLoader';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { animateScroll as scroll } from 'react-scroll';
+import { Message } from './Message/Message';
 import { Box } from '../App.styled';
 import { Api } from './Api/Api';
 import { FormSerch } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Modal } from './Modal/Modal';
 import { LoadMoreBtn } from './Button/Button';
+import { useLayoutEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    showBtn: false,
-    showModal: false,
-    showSpiner: false,
-    serchQuery: '',
-    page: 1,
-    data: [],
-    largePage: '',
-  };
+export const App = () => {
+  const [showBtn, setShowBtn] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [showSpiner, setShowSpiner] = useState(false);
+  const [serchQuery, setSerchQuery] = useState('');
+  const [largePage, setLargePage] = useState('');
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
 
- async componentDidUpdate(_, prevState) {
-    const { serchQuery, page } = this.state;
+  useEffect(() => {
+    if (serchQuery === '') return;
+    setShowSpiner(true);
+    Api(serchQuery, page)
+      .then(({ data: { hits, totalHits } }) => {
+        setData(prevState => prevState.concat(hits));
+        setShowBtn(true);
+        Message(page, totalHits, hits, setShowBtn);
+      })
+      .catch(erore => {
+        console.log(erore.message);
+      })
+      .finally(() => {
+        setShowSpiner(false);
+      });
+    return () => {};
+  }, [page, serchQuery]);
 
-    if (prevState.page !== page || prevState.serchQuery !== serchQuery) {
-      this.setState({ showSpiner: !this.state.showSpiner });
 
-      await Api(serchQuery, page)
-        .then(({ hits, totalHits }) => {
-          this.setState(prevState => ({ data: prevState.data.concat(hits) }));
-          this.setState({ showBtn: true });
-
-          if (this.state.page * 12 > +totalHits) {
-            this.setState({ showBtn: false });
-          }
-
-          if (totalHits === 0) {
-            return toast.error(
-              'Sorry, there are no images matching your search query. Please try again.',
-              {
-                theme: 'dark',
-              }
-            );
-          }
-
-          if (totalHits < 12 && hits.length === 0) {
-            return toast.warn(
-              'Were sorry, but youve reached the end of search results.',
-              {
-                theme: 'dark',
-              }
-            );
-          }
-        })
-        .catch(error => {
-          return console.log(error);
-        });
-
-      this.setState({ showSpiner: !this.state.showSpiner });
-    }
-  };
-
-  handeClick = e => {
-    e.preventDefault();
-    this.setState({
-      largePage: e.currentTarget.name,
-      showModal: !this.state.showModal,
-    });
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  handleSubmit = inputQuery => {
-    this.setState({
-      serchQuery: inputQuery,
-      page: 1,
-      data: [],
-    });
-  };
-
-  handleClickLoadMoreBtn = e => {
-    this.setState({ page: this.state.page + 1 });
+  useLayoutEffect(() => {
     scroll.scrollMore(1150, {
       smooth: true,
     });
+  }, [page, serchQuery]);
+
+
+const handeClick = e => {
+    e.preventDefault();
+    setLargePage(e.currentTarget.name);
+    setShowModal(!showModal);
   };
 
-  render() {
-    const { data, largePage, showModal, showSpiner, showBtn } = this.state;
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleSubmit = inputQuery => {
+    setSerchQuery(inputQuery);
+    setPage(1);
+    setData([]);
+  };
+
+  const handleClickLoadMoreBtn = e => {
+    setPage(prevs => prevs + 1);
+  };
 
     return (
       <Box>
-        <FormSerch onSubmit={this.handleSubmit} />
-        <ImageGallery onOpenModal={this.handeClick} data={data} />
-        {showBtn && <LoadMoreBtn onClick={this.handleClickLoadMoreBtn} />}
-        {showModal && <Modal onClick={this.toggleModal}>{largePage}</Modal>}
+        <FormSerch onSubmit={handleSubmit} />
+        <ImageGallery onOpenModal={handeClick} data={data} />
+        {showBtn && <LoadMoreBtn onClick={handleClickLoadMoreBtn} />}
+        {showModal && <Modal onClick={toggleModal}>{largePage}</Modal>}
         {showSpiner && <ClipLoader color="#00BFFF" size={100} />}
         <ToastContainer autoClose={3000} />
       </Box>
     );
-  }
+  
 }
